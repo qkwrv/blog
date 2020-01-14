594b89bc976121d6d18d07b72d0eafdf38f551f0
Jesse Johnson <jesserjohnson@gmail.com>
2020-01-13 21:34:54 -0800

    Initial commit: got scully working

---
+++ blog/first-post.md
@@ -0,0 +1,9 @@
+---
+title: First post
+description: blog description
+publish: false
+---
+
+<h1>Hello</h1>
+
+# First post a
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { IdleMonitorService, ScullyRoutesService } from "@scullyio/ng-lib";
+import { Component } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'qkwrv';
+  links$: Observable<any> = this.scully.available$;
+
+  constructor(private scully: ScullyRoutesService) {}
+
+  ngOnInit() {
+    // debug current pages
+    this.links$.subscribe(links => {
+      console.log(links);
+    });
+  }
 }
